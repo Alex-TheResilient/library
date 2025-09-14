d9013c7f6aa47fa54d28992ed82bde9edacd7f33
Alex Huaracha <129701149+Alex-TheResilient@users.noreply.github.com>
2025-09-14 17:19:31 -0500

    feat: Update Book constructor to include read status and enhance addBookToLibrary functionality

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const form = document.querySelector('#book-form');
 
 const myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, read) {
   if (!new.target) {
     throw new Error('Book constructor must be called with "new"');
   }
@@ -12,16 +12,21 @@ function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = false;
+  this.read = read;
   this.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${
-      this.read ? 'read' : 'not read yet'
+      this.read === 'read'
+        ? 'read'
+        : this.read === 'reading'
+        ? 'reading'
+        : 'not read yet'
     }`;
   };
 }
 
-function addBookToLibrary() {
-  // take params, create a book then store it in the array
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+  console.log(`Book added: ${book.info()}`);
 }
 
 document.querySelector('#add-book').addEventListener('click', () => {
@@ -29,5 +34,20 @@ document.querySelector('#add-book').addEventListener('click', () => {
 });
 
 document.querySelector('#close-modal').addEventListener('click', () => {
+  document.querySelector('#modal').classList.add('hidden');
+});
+
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const title = formData.get('title');
+  const author = formData.get('author');
+  const pages = formData.get('pages');
+  const readStatus = formData.get('read-status');
+  const newBook = new Book(title, author, pages);
+  newBook.read = readStatus;
+  console.log(newBook);
+  addBookToLibrary(newBook);
+  form.reset();
   document.querySelector('#modal').classList.add('hidden');
 });
