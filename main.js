d3ad536792d8a5a66b39f971dccd2199c9484887
Alex Huaracha <129701149+Alex-Huaracha@users.noreply.github.com>
2025-09-30 16:53:39 -0500

    feat: Enhance book form validation with custom messages and real-time feedback

--- main.js
+++ main.js
@@ -13,10 +13,6 @@ function defaultBooks() {
   renderLibrary();
 }
 
-window.onload = function () {
-  defaultBooks();
-};
-
 class Book {
   constructor(title, author, pages, read) {
     this.id = crypto.randomUUID();
@@ -108,7 +104,64 @@ function renderLibrary() {
 function addBookToLibrary(book) {
   myLibrary.push(book);
   renderLibrary();
-  // console.log(`Book added: ${book.info()}`);
+}
+
+function setCustomValidationMessages() {
+  const titleInput = document.querySelector('#title');
+  const authorInput = document.querySelector('#author');
+  const pagesInput = document.querySelector('#pages');
+
+  titleInput.addEventListener('input', function () {
+    if (this.validity.valueMissing) {
+      this.setCustomValidity('The book title must be filled!');
+    } else if (this.validity.tooShort) {
+      this.setCustomValidity('Title must be at least 2 characters long!');
+    } else if (this.validity.tooLong) {
+      this.setCustomValidity('Title is too long! Maximum 100 characters.');
+    } else {
+      this.setCustomValidity('');
+    }
+  });
+
+  authorInput.addEventListener('input', function () {
+    if (this.validity.valueMissing) {
+      this.setCustomValidity('The author name must be filled!');
+    } else if (this.validity.tooShort) {
+      this.setCustomValidity('Author name must be at least 2 characters long!');
+    } else if (this.validity.tooLong) {
+      this.setCustomValidity('Author name is too long! Maximum 50 characters.');
+    } else {
+      this.setCustomValidity('');
+    }
+  });
+
+  pagesInput.addEventListener('input', function () {
+    if (this.validity.valueMissing) {
+      this.setCustomValidity('Number of pages must be filled!');
+    } else if (this.validity.rangeUnderflow) {
+      this.setCustomValidity('Number of pages must be at least 1!');
+    } else if (this.validity.rangeOverflow) {
+      this.setCustomValidity(
+        'Number of pages seems unrealistic! Maximum 10,000.'
+      );
+    } else if (this.validity.badInput) {
+      this.setCustomValidity('Please enter a valid number for pages!');
+    } else {
+      this.setCustomValidity('');
+    }
+  });
+}
+
+function setupRealTimeValidation() {
+  const inputs = document.querySelectorAll('#book-form input');
+
+  inputs.forEach((input) => {
+    input.addEventListener('blur', function () {
+      if (!this.checkValidity()) {
+        this.reportValidity();
+      }
+    });
+  });
 }
 
 document.querySelector('#add-book').addEventListener('click', () => {
@@ -121,15 +174,32 @@ document.querySelector('#close-modal').addEventListener('click', () => {
 
 document.querySelector('#book-form').addEventListener('submit', (e) => {
   e.preventDefault();
-  const formData = new FormData(e.target);
-  const title = formData.get('title');
-  const author = formData.get('author');
-  const pages = formData.get('pages');
+
+  const form = e.target;
+
+  if (!form.checkValidity()) {
+    form.reportValidity();
+    return;
+  }
+
+  const formData = new FormData(form);
+  const title = formData.get('title').trim();
+  const author = formData.get('author').trim();
+  const pages = parseInt(formData.get('pages'));
   const readStatus = formData.get('read-status');
-  const newBook = new Book(title, author, pages);
-  newBook.read = readStatus;
-  console.log(newBook);
+
+  const newBook = new Book(title, author, pages, readStatus);
   addBookToLibrary(newBook);
   form.reset();
+
+  const inputs = form.querySelectorAll('input');
+  inputs.forEach((input) => input.setCustomValidity(''));
+
   document.querySelector('#modal').classList.add('hidden');
+});
+
+window.addEventListener('load', function () {
+  setCustomValidationMessages();
+  setupRealTimeValidation();
+  defaultBooks();
 });
